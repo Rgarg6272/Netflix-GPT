3278c5d2bcec2a42c98663c706584fde5bff090b
unknown <rgarg6272@gmail.com>
2024-05-04 15:59:11 +0530

    Language feature added

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { lang } from "../utils/languageConstant";
+import { useSelector } from "react-redux";
 
 const GptSearchBar = () => {
+  const langKey = useSelector((store) => store.config.lang);
+
   return (
     <div className="pt-[8%] flex justify-center">
       <form className="w-1/2 bg-black flex">
         <input
           type="text"
           className="p-4 m-4 w-[80%]"
-          placeholder={lang.hindi.gptSearchPlaceholder}
+          placeholder={lang[langKey].gptSearchPlaceholder}
         />
         <button className="py-2 m-4 px-4 w-[20%] rounded-lg bg-red-700">
-          {lang.hindi.search}
+          {lang[langKey].search}
         </button>
       </form>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -50,26 +50,29 @@ const Header = () => {
   const handleLangChange = (e) => {
     dispatch(changeLanguage(e.target.value));
   };
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between ">
       <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
-          <select
-            className="p-2 m-2 bg-gray-900 text-white"
-            onChange={handleLangChange}
-          >
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
-          </select>
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLangChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             className="px-4 py-2 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearch}
           >
-            GPT Search
+            {showGptSearch ? "HomePage" : "GPT Search"}
           </button>
           <img className="w-12 h-12" alt="usericon" src={user?.photoURL} />
           <button className="font-bold text-white" onClick={handleSignOut}>
